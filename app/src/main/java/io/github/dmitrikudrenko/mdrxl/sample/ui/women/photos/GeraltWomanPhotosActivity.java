70fcee8fe0d36fe5fc04e6ed74bf82dc9bf150ed
delphin <delphin@yandex-team.ru>
2017-12-27 13:46:14 +0300

    hide action bar in photo gallery

--- app/src/main/java/io/github/dmitrikudrenko/mdrxl/sample/ui/women/photos/GeraltWomanPhotosActivity.java
+++ app/src/main/java/io/github/dmitrikudrenko/mdrxl/sample/ui/women/photos/GeraltWomanPhotosActivity.java
@@ -2,6 +2,7 @@ package io.github.dmitrikudrenko.mdrxl.sample.ui.women.photos;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import io.github.dmitrikudrenko.mdrxl.sample.ui.base.BaseFragmentHolderRxActivity;
 
 public class GeraltWomanPhotosActivity extends BaseFragmentHolderRxActivity<GeraltWomanPhotosFragment> {
@@ -12,5 +13,13 @@ public class GeraltWomanPhotosActivity extends BaseFragmentHolderRxActivity<Gera
     @Override
     protected GeraltWomanPhotosFragment createFragment() {
         return GeraltWomanPhotosFragment.create();
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
